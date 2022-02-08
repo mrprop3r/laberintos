2def5e1e52e363d1978ec1d77f024c11a83077c2
Alberto Sanchez <mrprop3r@gmail.com>
2022-02-08 19:53:13 +0100

    version 0.1

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const LM = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ LM.abilities = {
+  "str": "LM.AbilityStr",
+  "dex": "LM.AbilityDex",
+  "con": "LM.AbilityCon",
+  "int": "LM.AbilityInt",
+  "wis": "LM.AbilityWis",
+  "cha": "LM.AbilityCha"
+};
+
+LM.abilityAbbreviations = {
+  "str": "LM.AbilityStrAbbr",
+  "dex": "LM.AbilityDexAbbr",
+  "con": "LM.AbilityConAbbr",
+  "int": "LM.AbilityIntAbbr",
+  "wis": "LM.AbilityWisAbbr",
+  "cha": "LM.AbilityChaAbbr"
+};
